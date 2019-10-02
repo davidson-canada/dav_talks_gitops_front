be2d76197d9afc9dbe2548aafa487eb17ffced3a
davidson-canada-devops <alexandre.theve@davidson.group>
2019-10-01 20:06:01 -0400

    maj url staging

--- config/staging.env.js
+++ config/staging.env.js
@@ -1,6 +1,6 @@
 'use strict'
 module.exports = {
   NODE_ENV: '"staging"',
-  ROOT_API: '"http://localhost:3000/api/url/"',
+  ROOT_API: '"http://gitops-front-staging.dav-poutine.ca/api/url/"',
   TITLE_APP: '"Davidson Staging"'
 }
